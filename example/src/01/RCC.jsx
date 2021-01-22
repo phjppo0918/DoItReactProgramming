5f5a0010d25e45d10d1a99430e0c46feff93cf81
phjppo0918 <phjppo0918@kpu.ac.kr>
2021-01-22 14:07:41 +0000

    chap3

--- example/src/01/RCC.jsx
+++ example/src/01/RCC.jsx
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 
 class RCC extends Component {
 	render(){
-		var text = "text Massage";
+		var text = 'text Massage';
 		return <div name='name'>{text}</div>;
 	}
 }
---
+++ example/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropComponent from './03/PropsComponent';
+
+class App extends React.Component {
+	render(){
+		return (
+			<PropComponent name="do it react"/>
+		);
+	}
+}
+
+export default App;
