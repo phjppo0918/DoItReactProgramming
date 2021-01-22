94ff7e6e566f55a0dda97a139b1f386725658e25
phjppo0918 <phjppo0918@kpu.ac.kr>
2021-01-22 11:43:36 +0000

    page 38

---
+++ example/src/01/RCC.jsx
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+class RCC extends Component {
+	render(){
+		var text = "text Massage";
+		return <div name='name'>{text}</div>;
+	}
+}
+
+export default RCC;
