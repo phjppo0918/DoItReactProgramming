880ddada3fa43eef6f088544b793644a936d0973
phjppo0918 <phjppo0918@kpu.ac.kr>
2021-01-22 14:54:20 +0000

    page 102

---
+++ example/src/03/ChildComponent2.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from  'prop-types';
+
+class ChildComponent2 extends React.Component {
+	render(){
+		const {
+			objValue,
+		} = this.props;
+		
+		return (
+		<div>
+			<div>객체값: {String(Object.entries(objValue))}</div>	
+		</div>
+		);
+	}
+}
+
+ChildComponent2.propTypes = {
+	//객체 프로퍼티
+	objValue: PropTypes.shape({
+		name: PropTypes.string,
+		age: PropTypes.number,
+	}),
+	
+	//필수 프로퍼티
+	requiredStringValue: PropTypes.string.isRequired,
+}
+
+export default ChildComponent2;
--- example/src/App.jsx
+++ example/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import BooleanComponent from './03/BooleanComponent';
+import ChildComponent2 from './03/ChildComponent2';
+//import BooleanComponent from './03/BooleanComponent';
 //import ChildComponent from './03/ChildComponent';
 //import PropComponent from './03/PropsComponent';
 
@@ -13,11 +14,18 @@ class App extends React.Component {
 		*/
 		
 		return (
-			
+			<div>
+				<ChildComponent2
+					objValue={{age: 20}}
+					requiredStringValue="문자"
+				/>
+			</div>
+			/*
 			<div>
 				<div><b>지루할 때:</b><BooleanComponent bored/></div>
 				<div><b>지루할 때:</b><BooleanComponent /></div>
 			</div>
+			*/
 			/*
 			<ChildComponent
 				boolValue={true}
