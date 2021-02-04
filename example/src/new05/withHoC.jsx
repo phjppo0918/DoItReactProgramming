c474f9a0acc094546104461d5ea4002045ed0ca0
Hyeonjun Park <phjppo0918@kpu.ac.kr>
2021-02-04 22:54:40 +0900

    하이어오더 컴포넌트 복습: page 245

--- example/src/new05/withHoC.jsx
+++ example/src/new05/withHoC.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 
 export default function withHoC(WrappedComponent) {
+    const { displayName, name} = WrappedComponent;
+    const WrappedComponentName = displayName || name;
     return class WithHoC extends React.Component {
+        static displayName = `withHoC(${WrappedComponentName})`;
         render() {
             return <WrappedComponent {...this.porps}/>;
         }
