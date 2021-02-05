3da3d5e43291711f052840f3f0b22c1a69705deb
phjppo0918 <phjppo0918@kpu.ac.kr>
2021-02-05 22:50:58 +0900

    하이어오더 컴포넌트 복습: page 257

--- example/src/new05/withState.jsx
+++ example/src/new05/withState.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import withState from 'recomepose/withState';
+import withState from 'recompose/withState';
 import Button from '../04/Button';
 
 export const withCountState = withState('count', 'setCount', 0);
@@ -16,3 +16,5 @@ function Counter({count, setCount}) {
         </div>
     );
 }
+
+export const CounterWithCountState = withCountState(Counter);
