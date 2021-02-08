e2760b78be09766e317137d092d85866c298d431
phjppo0918 <phjppo0918@kpu.ac.kr>
2021-02-08 13:48:15 +0000

    page 287

--- example/src/new06/LoadingProviderWithKey.jsx
+++ example/src/new06/LoadingProviderWithKey.jsx
@@ -1,42 +1,42 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export const DEFAULT_KEY = 'defaultLoadingKey'; //1
-export const contextPropTypes = { //2
-	loading: PropTypes.bool,
-	setLoading: PropTypes.func,
+export const DEFAULT_KEY = 'defaultLoadingKey';
+export const contextPropTypes = {
+  loading: PropTypes.bool,
+  setLoading: PropTypes.func,
 };
 
-export default (contextKey = DEFAULT_KEY) => { //3
-	class LoadingProvider extends React.Component {
-		constructor(props) {
-			super(props);
-			
-			this.state = {loading: false};
-			this.setLoading = this.setLoading.bind(this);
-		}
-		
-		getChildContext() {
-			return {
-				[contextKey]: { //4
-					loading:this.state.loading,
-					setLoading:this.setLoading,
-				},
-			};
-		}
-		
-		setLoading(loading) {
-			this.setState({loading});
-		}
-		
-		render() {
-			return this.props.children;
-		}
-	}
-	
-	LoadingProvider.childContextTypes = {
-		[contextKey]: contextPropTypes,
-	};
-	
-	return LoadingProvider;
+export default (contextKey = DEFAULT_KEY) => {
+  class LoadingProvider extends React.Component {
+    constructor(props) {
+      super(props);
+
+      this.state = { loading: false };
+      this.setLoading = this.setLoading.bind(this);
+    }
+
+    getChildContext() {
+      return {
+        [contextKey]: {
+          loading: this.state.loading,
+          setLoading: this.setLoading,
+        },
+      };
+    }
+
+    setLoading(loading) {
+      this.setState({ loading });
+    }
+
+    render() {
+      return this.props.children;
+    }
+  }
+
+  LoadingProvider.childContextTypes = {
+    [contextKey]: contextPropTypes,
+  };
+
+  return LoadingProvider;
 };
--- example/src/stories/ContextStory.jsx
+++ example/src/stories/ContextStory.jsx
@@ -3,7 +3,9 @@ import {storiesOf} from '@storybook/react';
 
 import HomePageComponent from '../new06/HomePageComponent';
 import HomePageWithProvider from '../new06/HomePageWithProvider';
+import HomePageWithTwoProvider from '../new06/HomePageWithTwoProvider';
 
 storiesOf('HomePageComponent', module)
 	.addWithJSX('컨텍스트 예제', () => <HomePageComponent/>)
-	.addWithJSX('Provider 예제', () => <HomePageWithProvider/>);
+	.addWithJSX('Provider 예제', () => <HomePageWithProvider/>)
+	.addWithJSX('이중 Provider 예제', () => <HomePageWithTwoProvider/>);
