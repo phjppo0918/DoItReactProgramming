d93fdc7eb7a6780ff139f8a02836415f7ea869a8
phjppo0918 <phjppo0918@kpu.ac.kr>
2021-02-09 23:14:56 +0900

    page 305

--- example/src/stories/ModalStory.jsx
+++ example/src/stories/ModalStory.jsx
@@ -4,6 +4,8 @@ import {storiesOf} from '@storybook/react';
 import Modal from '../06/modal/Modal';
 import Button from '../04/Button';
 import Text from '../04/Text';
+import ButtonWithModal from '../06/modal/ButtonWithModal';
+import ModalProvider, { Consumer } from '../06/modal/ModalProvider';
 
 storiesOf('Modal', module)
     .addWithJSX('기본 설정', () => (
@@ -16,4 +18,17 @@ storiesOf('Modal', module)
             <Button primary> 예</Button>
             <Button>닫기</Button>
         </Modal>
+    ))
+    .addWithJSX('ButtonWithModal', () => <ButtonWithModal/>)
+    .addWithJSX('ModalProvider', () => (
+        <ModalProvider>
+            <div>
+                <Text>
+                    다음 버튼을 눌러 모달을 실행합니다
+                </Text>
+                <Consumer>
+                    {({openModal}) => <Button onPress={()=> openModal()}>삭제</Button>}
+                </Consumer>
+            </div>
+        </ModalProvider>
     ));
