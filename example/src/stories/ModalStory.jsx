a343776ffec579a16cd0c186dcd1417b01f13e58
phjppo0918 <phjppo0918@kpu.ac.kr>
2021-02-11 10:35:02 +0000

    page 313

--- example/src/stories/ModalStory.jsx
+++ example/src/stories/ModalStory.jsx
@@ -7,7 +7,11 @@ import Text from '../04/Text';
 import ButtonWithModal from '../06/modal/ButtonWithModal';
 import ModalProvider, { Consumer } from '../06/modal/ModalProvider';
 
-import ModalProviderWithKey, {CONFIRM_DELETE_MODAL} from '../06/modal/ModalProviderWithKey';
+import ModalProviderWithKey, 
+		{
+			CONFIRM_DELETE_MODAL,
+		 	CREATE_MEMBER_MODAL,
+		} from '../06/modal/ModalProviderWithKey';
 import {Consumer as ModalConsumer} from '../06/modal/ModalContext';
 
 storiesOf('Modal', module)
@@ -48,4 +52,23 @@ storiesOf('Modal', module)
                 </ModalConsumer>
             </div>
         </ModalProviderWithKey>
-    ));
+    ))
+	.addWithJSX('ModalProviderWithKey2',() => (
+		<ModalProviderWithKey>
+			<div>
+				<Text>
+					다음 버튼 눌러 모달을 실행합니다.
+				</Text>
+				<ModalConsumer>
+					{({openModal}) => (
+						<Button onPress={()=> openModal(CONFIRM_DELETE_MODAL)}> 모달 열기</Button>
+					)}
+				</ModalConsumer>
+				<ModalConsumer>
+					{({ openModal }) => (
+						<Button onPress={() => openModal(CREATE_MEMBER_MODAL)}>회원 가입</Button>
+					)}
+				</ModalConsumer>
+			</div>
+		</ModalProviderWithKey>
+	));
