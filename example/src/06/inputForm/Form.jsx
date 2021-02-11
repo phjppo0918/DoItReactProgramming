8ec54da8320ec83c1413aa4f69023f2ff411f046
phjppo0918 <phjppo0918@kpu.ac.kr>
2021-02-11 14:30:42 +0000

    page 325

--- example/src/06/inputForm/Form.jsx
+++ example/src/06/inputForm/Form.jsx
@@ -1,53 +1,76 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import {Provider} from './FormContext';
+import { Provider } from './FormContext';
 
-export default class FormProvider extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			values: {},
-			errors: {},
-		};
-		this.reset = this.reset.bind(this);
-		this.onChange = this.onChange.bind(this);
-		this.submit = this.submit.bind(this);
-	}
-	onChange(name, updateValue) {
-		this.setState(({values}) => ({
-			values: {
-				...values,
-				[name]: updateValue,
-			},
-		}), () => this.validate(this.state.values));
-	}
-	reset() {
-		this.setState({values: {}, errors: {}});
-	}
-	submit() {
-		this.props.onSubmit(this.state.values);
-	}
-	validate(values) {
-		const {validate } = this.props;
-		if(!validate) {
-			return;
-		}
-		const errors = validate(values);
-		this.setState(({
-			errors,
-		}));
-	}
-	render() {
-		const {values, errors} = this.state;
-		return (
-			<Provider
-				value={{
-					errors,
-					values,
-					onChange: this.onChange,
-					reset: this.reset,
-				}}
-		)
-	}
+class FormProvider extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      values: {},
+      errors: {},
+    };
+    this.reset = this.reset.bind(this);
+    this.onChange = this.onChange.bind(this);
+    this.submit = this.submit.bind(this);
+  }
+
+  onChange(name, updatedValue) {
+    this.setState(
+      ({ values }) => ({
+        values: {
+          ...values,
+          [name]: updatedValue,
+        },
+      }),
+      () => this.validate(this.state.values),
+    );
+  }
+
+  reset() {
+    this.setState({ values: {}, errors: {} });
+  }
+
+  submit() {
+    this.props.onSubmit(this.state.values);
+  }
+
+  validate(values) {
+    const { validate } = this.props;
+    if (!validate) {
+      return;
+    }
+    const errors = this.props.validate(values);
+    this.setState({
+      errors,
+    });
+  }
+
+  render() {
+    const { values, errors } = this.state;
+    return (
+      <Provider
+        value={{
+          errors,
+          values,
+          onChange: this.onChange,
+          reset: this.reset,
+          submit: this.submit,
+        }}
+      >
+        {this.props.children}
+      </Provider>
+    );
+  }
 }
+
+FormProvider.propTypes = {
+  validate: PropTypes.func,
+  onSubmit: PropTypes.func.isRequired,
+};
+
+FormProvider.defaultProps = {
+  validate: () => ({}),
+};
+
+export default FormProvider;
