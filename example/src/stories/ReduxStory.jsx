2454b0c293b3a2a9f705726c777f01e9618c693e
phjppo0918 <phjppo0918@kpu.ac.kr>
2021-02-12 09:58:25 +0000

    bug fix

--- example/src/stories/ReduxStory.jsx
+++ example/src/stories/ReduxStory.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import {storiesOf} from '@storybook/react';
+import { storiesOf } from '@storybook/react';
 
-import {ReduxApp} from '../07/ReduxApp';
+import ReduxApp01 from '../07/ReduxApp01';
+
 
 storiesOf('ReduxApp', module)
-	.addWithJSX('ReduxApp example', () => <ReduxApp/>);
+  .addWithJSX('기본 스토어 설정', () => <ReduxApp01 />);
